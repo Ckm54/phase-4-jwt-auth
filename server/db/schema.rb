45e0c5689270b09cee8a4fbe6c3a92093646aacf
collo <mutaic84@gmail.com>
2022-07-27 14:40:19 +0300

    jwt auth lab code solution

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_143839) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_09_16_143839) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.string "bio"
     t.string "avatar"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
